2eee6f5d3834839998df52d7d8db95c1f9f54364
Tristan Beasley <twbeasley13@gmail.com>
2018-12-20 21:52:42 -0500

    add more heroku changes

--- db/migrate/20181210230504_add_categories_table.rb
+++ db/migrate/20181210230504_add_categories_table.rb
@@ -2,7 +2,8 @@ class AddCategoriesTable < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
       t.string :name,
-      t.integer :category_id
+      t.integer :category_id,
+      t.integer :blah
     end
   end
 end
